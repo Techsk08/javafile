5cef863740260f6a5d469356cb76d30039b211e9
Techsk08 <133515659+Techsk08@users.noreply.github.com>
2023-07-22 00:52:10 +0530

    Add files via upload

---
+++ javaloops.java
@@ -0,0 +1,70 @@
+
+public class javaloops {
+public static void main(String[] args) {
+    //int a = 7;
+  //  int b = 8;
+//for(int i=1;i<=a;i++) {
+   // for(int j= 1;j<=b;j++) {
+     //if(i==1|| j==1||i==a||j==b) {
+       // System.out.print("*");
+     //}else{
+//System.out.print(" ");
+  //   }
+    // System.out.println();
+    //}
+//System.out.println();
+//int a = 9;
+//for(int i=a; i>=1; i--) {
+ // for(int  j=1; j<=i; j++) {
+ //     System.out.print("*");
+ //   }
+//System.out.println();
+//}
+
+//int s = 8;
+//for(int i=1;i<=s;i++){
+  //  for(int j=1; j<=s-i ;j++) {
+    //    System.out.print(" ");
+   
+// }
+//for(int j=1; j<= i; j++) {
+  //  System.out.print(j);
+//}
+//System.out.println();
+//}
+//int number =0;
+int s =9;
+for(int i=1; i<=s; i++) {
+    for(int j=1; j<=i; j++) {
+        System.out.print("*");
+    
+    }
+int spaces = 2 *(s-i);
+for(int j=1;j<=spaces;J++) {
+  System.out.print(" "); 
+}
+}
+for(int j=1;j<=i;j++) {
+  System.out.print("*");
+}
+System.out.println();
+
+
+for(int i=s; i<=i; i--) {
+  for(int j=1; j<=i; j++) {
+      System.out.print("*");
+  
+  }
+int spaces = 2 *(s-i);
+for(int j=1;j<=spaces;J++) {
+System.out.print(" "); 
+}
+}
+for(int j=1;j<=i;j++) {
+System.out.print("*");
+}
+System.out.println();
+}   
+}
+    
+
