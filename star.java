eb31179619f92b00d087be94aa9048254db9fc2b
Techsk08 <133515659+Techsk08@users.noreply.github.com>
2023-07-25 23:41:04 +0530

    Add files via upload

---
+++ star.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class star {
+     public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+System.out.println("hi sittu");
+
+}
+
+    }
+
